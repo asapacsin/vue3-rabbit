0bd36e63d5cadb60abda326fad9e49938ebd8440
asapacsin <asapacsin@gmail.com>
2023-08-10 15:13:24 +0800

    api

--- src/main.js
+++ src/main.js
@@ -6,6 +6,12 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+//test api function
+import {getCategory} from '@/api/testAPI.js'
+
+getCategory().then(res=>{
+    console.log(res)
+})
 const app = createApp(App)
 
 app.use(createPinia())
